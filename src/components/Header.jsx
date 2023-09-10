9cb90c942cee505f9d9e0705aa156d6309ae5443
Victor Torres <vatorresm88@gmail.com>
2023-09-10 13:51:19 -0500

    Trabajando en el Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import logo from '../assets/logo.png';
 
-export default function Header() {
+export default function Header(props) {
+  const navigate = useNavigate();
   return (
-    <div>Header</div>
-  )
+    <Container>
+      <div className='logo'>
+        <img src={logo} alt='logo' />
+      </div>
+      <button onClick={() => navigate(props.login ? '/login' : '/signup')}>
+        {props.login ? 'Log In' : 'Sing In'}
+      </button>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  padding: 0 4rem;
+  .logo {
+    img {
+      height: 5rem;
+    }
+  }
+  button {
+    padding: 0.5rem 1rem;
+    background-color: #e50914;
+    border: none;
+    cursor: pointer;
+    color: white;
+    border-radius: 0.2rem;
+    font-weight: bolder;
+    font-size: 1.05rem;
+  }
+`;
