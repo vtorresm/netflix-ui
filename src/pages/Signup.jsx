67ed44c85b6a9540ece97415ea99b3b99eb070cc
Victor Torres <vatorresm88@gmail.com>
2023-09-16 15:04:01 -0500

    Guardando usuario en Firebase

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import BackgroundImage from '../components/BackgroundImage';
 import Header from '../components/Header';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { firebaseAuth } from '../utils/firebase-config';
 
 export default function Signup() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const hadleSignIn = async () => {
+    try {
+      const { email, password } = formValues;
+
+      await createUserWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <Container>
+    <Container showPassword={showPassword}>
       <BackgroundImage />
       <div className='content'>
-        <Header />
+        <Header login />
         <div className='body flex column a-center j-center'>
           <div className='text flex column'>
             <h1>Unlimited movies, TV shows and more</h1>
@@ -18,11 +35,37 @@ export default function Signup() {
             </h6>
           </div>
           <div className='form'>
-            <input type='email' placeholder='Email Address' name='email' />
-            <input type='password' placeholder='Password' name='password' />
-            <button>Get Started</button>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={formValues.email}
+              onChange={(e) =>
+                setFormValues({
+                  ...formValues,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
+            {showPassword && (
+              <input
+                type='password'
+                placeholder='Password'
+                name='password'
+                value={formValues.password}
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+              />
+            )}
+            {!showPassword && (
+              <button onClick={() => setShowPassword(true)}>Get Started</button>
+            )}
           </div>
-          <button>Log In</button>
+          <button onClick={hadleSignIn}>Sign Up</button>
         </div>
       </div>
     </Container>
@@ -40,5 +83,51 @@ const Container = styled.div`
     width: 100vw;
     display: grid;
     grid-template-rows: 15vh 85vh;
+    .body {
+      gap: 1rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        h1 {
+          padding: 0 25rem;
+        }
+      }
+      .form {
+        display: grid;
+        grid-template-columns: ${({ showPassword }) =>
+          showPassword ? '1fr 1fr' : '2fr 1fr'};
+        width: 60%;
+        input {
+          color: black;
+          border: none;
+          padding: 1.5rem;
+          font-size: 1.2rem;
+          border: 1px solid black;
+          &:focus {
+            outline: none;
+          }
+        }
+        button {
+          padding: 0.5rem 1rem;
+          background-color: #e50914;
+          border: none;
+          cursor: pointer;
+          color: white;
+          font-weight: bolder;
+          font-size: 1.05rem;
+        }
+      }
+      button {
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        cursor: pointer;
+        color: white;
+        border-radius: 0.2rem;
+        font-weight: bolder;
+        font-size: 1.05rem;
+      }
+    }
   }
 `;
