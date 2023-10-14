644d467b28a9ba4cd0d564645c6f8bf67f7339b9
Victor Torres <vatorresm88@gmail.com>
2023-10-14 12:20:03 -0500

    Limpiando UserListedMovies.jsx

--- src/pages/UserListedMovies.jsx
+++ src/pages/UserListedMovies.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { onAuthStateChanged } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -25,7 +24,7 @@ export default function UserListedMovies() {
     if (email) {
       dispatch(getUsersLikedMovies(email));
     }
-  }, [email]);
+  }, [dispatch, email]);
 
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
